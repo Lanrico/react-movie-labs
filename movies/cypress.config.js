287a55f477c30383dd439175e0ff646e626cfe61
Guanlan Ji <228298909@qq.com>
2022-10-18 11:08:15 +0100

    E2E tests - Navigation test spec.

--- movies/cypress.config.js
+++ movies/cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
   e2e: {
     "baseUrl": "http://localhost:3000/",
+    "viewportWidth": 1980,
+    "viewportHeight": 1080,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
