79b808678e71890fead1ad6e167579040eabe663
Guanlan Ji <228298909@qq.com>
2022-10-24 12:52:25 +0100

    Fix upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,32 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import Spinner from '../components/spinner';
+import { useQuery } from 'react-query';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
 
 const UpcomingMoviesPage = (props) => {
 
-  // const movies = getUpcomingMovies(); 
-  const [movies, setMovies] = useState([]);
+  // const [movies, setMovies] = useState([]);
+  const {  data, error, isLoading, isError }  = useQuery('discoverUpcoming', getUpcomingMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+  
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
 
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
+  // const addToFavorites = (movieId) => {
+  //   const updatedMovies = movies.map((m) =>
+  //     m.id === movieId ? { ...m, favorite: true } : m
+  //   );
+  //   setMovies(updatedMovies);
+  // };
 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-  }, []);
+  // useEffect(() => {
+  //   getUpcomingMovies().then(movies => {
+  //     setMovies(movies);
+  //   });
+  // }, []);
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
-      selectFavorite={addToFavorites}
-    />
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}    />
   );
 };
 
